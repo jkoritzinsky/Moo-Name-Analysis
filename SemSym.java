4aaf4658818348cd6df9f7b533e27e61b35e983d
Nolan Tenpas <ntenpasdeveloper@gmail.com>
2017-03-27 17:49:15 -0500

    added StructDefSym and StructSym classes

--- SemSym.java
+++ SemSym.java
@@ -13,3 +13,47 @@ public class SemSym {
         return type;
     }
 }
+
+class StructDefSym {
+    private String name;
+    private SymTable fields;
+
+    public StructDefSym(String name, SymTable fields) {
+        this.name = name;
+        this.fields = fields;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public SymTable getFields() {
+        return this.fields;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
+
+class StructSym {
+    private String name;
+    private StructDefSym def;
+
+    public StructSym(String name, StructDefSym def) {
+        this.name = name;
+        this.def = def;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public StructDefSym getDef() {
+        return def;
+    }
+    
+    public String toString() {
+        return name;
+    }
+}
